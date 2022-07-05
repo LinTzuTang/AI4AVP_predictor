43ad16a099dc23dc82220689564b6c23d060ada4
root <root@gamera.iis.sinica.edu.tw>
2022-07-05 16:33:39 +0800

    update example

--- example/example.sh
+++ example/example.sh
@@ -1,2 +1,2 @@
 root=$(dirname "$0")
-python3 $root/../predictor.py -f $root/example.fasta -o $root/example_output.csv -m
+python3 $root/../predictor.py -f $root/example.fasta -o $root/example_output.csv -m 'n'
